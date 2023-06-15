0330b09e689bd98f0550768e60bee84e029dd031
Lemi Orhan Ergin <lemi@craftgate.io>
2023-06-15 19:39:17 +0300

    kuzey's first real code

--- src/main/groovy/com/kuzey/NumberGuessingGame.groovy
+++ src/main/groovy/com/kuzey/NumberGuessingGame.groovy
@@ -2,14 +2,17 @@ package com.kuzey
 
 BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))
 
+// create a new random number between 1-100
 int randomNumber = new Random().nextInt(100) + 1
+// player's guess
 int guess = 0
+// number of attempts that the player has
 int attempts = 0
 
 println "Welcome to the number guessing game!"
 println "I'm thinking of a number between 1 and 100."
 
-while (guess != randomNumber) {
+while (guess != randomNumber && attempts < 10) {
     print "Take a guess: "
 
     try {
@@ -28,4 +31,8 @@ while (guess != randomNumber) {
     }
 }
 
-println "Congratulations! You guessed the right number in $attempts attempts."
+if (attempts == 10) {
+    println "You got 10 attempts !GAME OVER!"
+} else {
+    println "Congratulations! You guessed the right number in $attempts attempts."
+}
